6868d685162cf30216dbbe6c5f4980a4ff91c39c
Ben Hougland <benjamin.hougland@gmail.com>
2020-03-05 23:22:04 -0500

    added more

--- volcanoes/src/volcanoes/core.clj
+++ volcanoes/src/volcanoes/core.clj
@@ -1,6 +1,8 @@
 (ns volcanoes.core
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io  :as io]))
+            [clojure.java.io  :as io]
+            [clojure.string   :as str]))
+
 
 (def csv-lines
   (with-open [csv (io/reader "/home/ben/Downloads/GVP_Volcano_List_Holocene.csv")]
@@ -26,16 +28,24 @@
            (zipmap header-line volcano-line))
          volcano-lines)))
 
+(defn slash->set [s]
+  (set (map str/trim (str/split s #"/"))))
+
 (defn parse-numbers [volcano]
   (-> volcano
       (update :elevation-meters #(Integer/parseInt %))
       (update :longitude #(Double/parseDouble %))
-      (update :latitude #(Double/parseDouble %))))
+      (update :latitude #(Double/parseDouble %))
+     #_(assoc :last-eruption-parsed (parse-eruption-date (:last-known-eruption volcano)))
+      (update :tectonic-setting slash->set)
+      (update :dominant-rock-type slash->set)))
 
 (def volcanoes-parsed
   (map parse-numbers volcano-records))
 
 (def types (set (map :primary-volcano-type volcano-records)))
+
+
 
 (comment
   ;; REPL-driven code
@@ -45,4 +55,15 @@
   (let [volcano (filter #(= "221291" (:volcano-number %)) volcanoes-parsed)]
     (clojure.pprint/pprint volcano)))
   ;;)
+
+;; run-length encoding
+;; [:a :a :a :b]
+;; [[3 :a] [1 :b]]
+(defn rle [l]
+  (map (juxt count first)(partition-by identity l)))
+
+(defn drop-every [n l]
+  (mapcat #(take (dec n) %)(partition-all n l)))
+
+
 
